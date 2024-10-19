0e4db9bd943db7f0f84f635a354cec69168eb7ac
AYUSHMAAN-B <ayushmaanbn@gmail.com>
2024-10-19 23:01:28 +0530

    Added option to select bill from gallary

--- MyExpenses/app/src/main/java/com/example/expensetracker/presentation/Home/CameraScreen.kt
+++ MyExpenses/app/src/main/java/com/example/expensetracker/presentation/Home/CameraScreen.kt
@@ -1,9 +1,19 @@
 package com.example.expensetracker.presentation.Home
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
 import android.graphics.Matrix
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Build
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
@@ -26,25 +36,39 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material.icons.filled.PhotoLibrary
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.ImageLoader
+import coil.request.ErrorResult
+import coil.request.ImageRequest
+import coil.request.SuccessResult
 import com.example.expensetracker.MyApplication
+import com.google.android.play.integrity.internal.c
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import java.io.IOException
+
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Destination
@@ -70,17 +94,34 @@ private fun CameraScreen(
 
     val controller = remember {
         LifecycleCameraController(context).apply {
-            setEnabledUseCases( CameraController.IMAGE_CAPTURE or CameraController.VIDEO_CAPTURE )
+            setEnabledUseCases( CameraController.IMAGE_CAPTURE )
         }
     }
+
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+
+    val photoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri ->
+            selectedImageUri = uri
+            uri?.let {
+                val bitmap = uriToBitmap(context, it)
+                if (bitmap != null) {
+                    capturedImage = bitmap
+                } else {
+                    Toast.makeText(context, "Failed to load image", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    )
+
 
     Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(8.dp)
     ) {
-        if( capturedImage != null )
-        {
+        if( capturedImage != null ) {
             ImagePreview(
                 bitmap = capturedImage!!,
                 onSave = {
@@ -92,8 +133,7 @@ private fun CameraScreen(
                 onCancel = { capturedImage = null }
             )
         }
-        else
-        {
+        else {
             CameraPreview(
                 controller = controller,
                 modifier = Modifier.fillMaxSize()
@@ -143,13 +183,52 @@ private fun CameraScreen(
                     Icon(
                         imageVector = Icons.Default.PhotoCamera,
                         contentDescription = null,
-                        modifier = Modifier.size(64.dp).padding(2.dp)
+                        modifier = Modifier
+                            .size(64.dp)
+                            .padding(2.dp)
+                    )
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .padding(bottom = 120.dp, end = 24.dp)
+                    .size(60.dp)
+                    .border(2.dp, Color.White, CircleShape)
+                    .align(Alignment.BottomEnd)
+            ) {
+                IconButton(
+                    onClick = { photoPickerLauncher.launch(
+                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                    ) },
+                    modifier = Modifier
+                        .size(54.dp)
+                        .align(Alignment.Center)
+                ) {
+                    Icon(Icons.Default.PhotoLibrary,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(54.dp)
+                            .padding(2.dp)
                     )
                 }
             }
         }
     }
 }
+
+fun uriToBitmap(context: Context, uri: Uri): Bitmap? {
+    return try {
+        val inputStream = context.contentResolver.openInputStream(uri)
+        BitmapFactory.decodeStream(inputStream).also {
+            inputStream?.close()
+        }
+    } catch (e: IOException) {
+        Log.e("CameraScreen", "Error converting URI to Bitmap", e)
+        null
+    }
+}
+
 
 @Composable
 fun ImagePreview(
